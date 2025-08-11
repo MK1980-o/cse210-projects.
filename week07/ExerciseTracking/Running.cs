62e9b619104aba62cd87c6e8dc9432c1b17613dd
MK1980-o <mkuriyazi@byupathway.edu>
2025-08-11 09:59:35 +0200

    cse210-projects

--- week07/ExerciseTracking/Running.cs
+++ week07/ExerciseTracking/Running.cs
@@ -1,6 +1,6 @@
 public class Running : Activity
 {
-    private double _distance; // miles
+    private double _distance; 
 
     public Running(string date, double length, double distance)
         : base(date, length)
